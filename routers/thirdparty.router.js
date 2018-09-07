59b0feeef4c3091246d5cba206d4f4871ee52470
Val Singhal <Vallsurf@gmail.com>
2018-09-07 15:23:06 -0700

    try new

--- routers/thirdparty.router.js
+++ routers/thirdparty.router.js
@@ -10,59 +10,51 @@ var rp = require('request-promise-native');
 
 router.use(jsonParser);
 
-router.route('/')
+router.route('/:spot')
     .get((req, res) => {
-        // let forecast
-        // let wind
-        // let tide
-        // let swell
-        // let urls = [
-        //     `http://api.spitcast.com/api/county/wind/orange-county/`,
-        //     `http://api.spitcast.com/api/county/swell/orange-county/`,
-        //     `http://api.spitcast.com/api/county/tide/orange-county`]
-        
-        //     var options = {
-        //         uri: 'http://api.spitcast.com/api/county/wind/orange-county/',
-        //         json: true // Automatically parses the JSON string in the response
-        //     };
-
-        //     var options2 = {
-        //         uri: 'http://api.spitcast.com/api/county/wind/los-angeles/',
-        //         json: true // Automatically parses the JSON string in the response
-        //     };
-             
-        //     Promise.all([
-        //         rp(options),
-        //         rp(options2)])
-        //         .then(function(first, second) {first.pipe(process.stdout)})
-        //         .catch(function (err) {
-        //             // API call failed...
-        //         });
-
-        let urls = [
-            `http://api.spitcast.com/api/county/wind/orange-county/`,
-            `http://api.spitcast.com/api/county/swell/orange-county/`,
-            `http://api.spitcast.com/api/county/tide/orange-county`]
         //get spot forecast first and handle if doesn't exist
         let forecast; 
-        fetch(`http://api.spitcast.com/api/spot/forecast/205/`)
+        fetch(`http://api.spitcast.com/api/spot/forecast/${req.params.spot}/`)
         .then(res => {
             if(!res.ok){return forecast=`nogood`}
             return res.json()})
         .then(forecast => {res.status(200).json(forecast)})
+
+        .catch(err => {return console.log(`err: ${err}`)})      
         
-        .catch(err => {return console.log(`err: ${err}`)})
-      //get County Data
-        // .then(Promise.all(urls.map(url => fetch(url))).then(([res1,res2,res3]) => Promise.all([res1.json(), res2.json(), res3.json()]))
-        // .then(([ wind, swell, tide]) => {
-                    
+        
+
     });
 
-router.route('/:county/:id')
+router.route('/spotdetails/:county/:spot')
     .get((req, res) => {
-    request({
-        uri: 'http://api.spitcast.com/api/county/spots/orange-county/',
-      }).pipe(res);
+
+        let urls = [
+            `http://api.spitcast.com/api/county/wind/${req.params.county}/`,
+            `http://api.spitcast.com/api/county/swell/${req.params.county}/`,
+            `http://api.spitcast.com/api/county/tide/${req.params.county}/`]
+        //get spot forecast first and handle if doesn't exist
+        let forecast; 
+        fetch(`http://api.spitcast.com/api/spot/forecast/${req.params.spot}/`)
+        .then(res => {
+            if(!res.ok){return forecast=`nogood`}
+            return res.json()})
+        .then(res => {forecast = res})
+        .catch(err => {return console.log(`err: ${err}`)})
+      //get County Data
+        .then(Promise.all(urls.map(url => fetch(url))).then(([res1,res2,res3]) => Promise.all([res1.json(), res2.json(), res3.json()]))
+        .then(([ wind, swell, tide]) => {
+            let response = {wind: [...wind],
+                            swell: [...swell],
+                            tide: [...tide],
+                            forecast: [...forecast]
+            } 
+            console.log(Object.keys(response.forecast).length)
+            res.status(200).json(response)
+ 
+            })
+        )
+      .catch(err => console.log(err));
     });
 
 module.exports = { router };
