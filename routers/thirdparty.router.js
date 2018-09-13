0ab84febc6792108d2429a8c209a5642f2fe90d2
Val Singhal <Vallsurf@gmail.com>
2018-09-13 16:49:33 -0700

    errorhandling

--- routers/thirdparty.router.js
+++ routers/thirdparty.router.js
@@ -2,7 +2,6 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const jsonParser = bodyParser.json();
 const router = express.Router();
-const request = require('request');
 const fetch = require('node-fetch')
 var rp = require('request-promise-native');
 
@@ -37,22 +36,38 @@ router.route('/spotdetails/:county/:spot')
         let forecast; 
         fetch(`http://api.spitcast.com/api/spot/forecast/${req.params.spot}/`)
         .then(res => {
-            if(!res.ok){return forecast=`nogood`}
-            return res.json()})
+            const contentType = res.headers.get("content-type"); 
+            if(contentType && contentType.indexOf("application/json") !== -1){
+            return res.json()}
+            else{
+                return forecast = [{key1 : 'test'}, {key2: 'test'}]
+            }
+        })
+            
         .then(res => {forecast = res})
         .catch(err => {return console.log(`err: ${err}`)})
-      //get County Data
-        .then(Promise.all(urls.map(url => fetch(url))).then(([res1,res2,res3]) => Promise.all([res1.json(), res2.json(), res3.json()]))
+        .then(Promise.all(urls.map(url => fetch(url))).then(([res1,res2,res3]) => Promise.all([res1.json(), res2.json(), res3.json()])) .catch(err => console.log(err))
         .then(([ wind, swell, tide]) => {
+            if(forecast){
             let response = {wind: [...wind],
                             swell: [...swell],
                             tide: [...tide],
                             forecast: [...forecast]
             } 
-            console.log(Object.keys(response.forecast).length)
+            
             res.status(200).json(response)
- 
-            })
+        }
+            else{
+                forecast = [{key1 : 'test'}, {key2: 'test'}]
+            let response = {wind: [...wind],
+                            swell: [...swell],
+                            tide: [...tide],
+                            forecast: [...forecast]
+                            } 
+            
+            res.status(200).json(response)
+        }
+            }).catch(err => console.log(err))
         )
       .catch(err => console.log(err));
     });
